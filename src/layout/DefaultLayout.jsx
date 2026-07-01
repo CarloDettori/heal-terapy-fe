77f512aba601f08c40c5259b3d6930279fcecbc5
CarloDettori <dettoric07@gmail.com>
2026-07-01 16:52:32 +0200

    top scroll at page change

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,12 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import { GlobalContext } from "../context/GlobalContext"
 import SidebarComponent from "../components/common/SidebarCompnent.jsx";
 export default function DefaultLayout() {
 
     const { sidebarLinks } = useContext(GlobalContext);
+    const scrollRef = useRef(null)
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        function scrollToTarget(target) {
+            if (!target) return
+            target.scrollIntoView({ behavior: "auto", block: "start" })
+        }
+
+        scrollToTarget(scrollRef.current)
+    }, [pathname])
 
     return (
         <>
@@ -14,6 +25,7 @@ export default function DefaultLayout() {
             <div className="flex">
                 <SidebarComponent infos={sidebarLinks} />
                 <main className="h-screen bg-(--lightest-theme) text-(--dark-theme) overflow-y-scroll hide-scrollbar w-full">
+                    <div ref={scrollRef}></div>
                     <Outlet />
                 </main>
             </div>
