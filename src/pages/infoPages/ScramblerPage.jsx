ef535edd808c07ad321b7791497311683d98866b
CarloDettori <dettoric07@gmail.com>
2026-07-08 19:52:34 +0200

    scrambler update

--- src/pages/infoPages/ScramblerPage.jsx
+++ src/pages/infoPages/ScramblerPage.jsx
@@ -160,19 +160,19 @@ export default function ContentPage() {
 
             <TextInfo customClass="font-bold text-page-size">SCRAMBLER THERAPY®</TextInfo>
             <img src="/scrambler-machine.png" alt="" className="flex mx-auto" />
-            <WhiteCard customClass="">La “Calmare® Scrambler Therapy” rappresenta un’interessante tecnologia di stimolazione nervosa non-invasiva destinata al trattamento del dolore neuropatico cronico e oncologico, molto valorizzata negli Stati Uniti per la sua proposta “senza farmaci” e “da ultimo tentativo”. È importante perché offre un’alternativa potenziale in un contesto – quello del dolore cronico – che ha molte lacune e grandi costi. È innovativa nel suo approccio (messaggi “no-pain” in luogo dei segnali di dolore), ma va usata con cautela: l’evidenza è ancora in fase di consolidamento, le risposte variano e la copertura assicurativa può essere problematica.</WhiteCard>
+            <WhiteCard customClass="">La Scrambler Therapy rappresenta un’interessante tecnologia di stimolazione nervosa non-invasiva destinata al trattamento del dolore neuropatico cronico e oncologico, molto valorizzata negli Stati Uniti per la sua proposta “senza farmaci”. È importante perché offre un’alternativa potenziale in un contesto – quello del dolore cronico – che ha molte lacune e grandi costi. È innovativa nel suo approccio (messaggi “no-pain” in luogo dei segnali di dolore), ma va usata, essendo una apparecchiatura biomedicale, solo esclusivamente da personale medico. </WhiteCard>
 
 
             <TextInfo size={1} customClass="font-bold"></TextInfo>
 
             <TextContainer customClass="gap-10">
 
-                <h1 className="text-center font-bold">Cosa si intende per “Calmare” / Scrambler Therapy</h1>
-                <h2>Elettrodi applicati sulla pelle inviano segnali a bassa intensità che “rimodulano” o “sovrascrivono” i segnali di dolore trasmessi dai nervi verso il cervello, facendo sì che il messaggio “dolore” venga sostituito da un messaggio “nessun dolore” o “non dolore”.
+                <h1 className="text-center font-bold">Cosa si intende per Scrambler Therapy</h1>
+                <h2>Elettrodi applicati sulla cute inviano segnali a bassa intensità che “rimodulano” e “sovrascrivono” i segnali di dolore trasmessi dalle fibre C sensitive verso l'encefalo, facendo sì che il messaggio “dolore” venga sostituito da un messaggio “nessun dolore” o “non dolore”.
 
                     <br /><br />
 
-                    Non è una stimolazione tradizionale tipo TENS (Transcutaneous Electrical Nerve Stimulation) ma, secondo i produttori e alcuni studi, un sistema che interviene sul “linguaggio” dell’informazione afferente del dolore (“no-pain message”).</h2>
+                    Non è una stimolazione tradizionale tipo TENS (Transcutaneous Electrical Nerve Stimulation) ma, secondo l'ingegnere Giuseppe Marineo  (inventore della scrambler therapy), questa terapia biomedica rappresenta un sistema che interviene sul “linguaggio” dell’informazione afferente del dolore (“no-pain message”).</h2>
             </TextContainer>
 
             {isDesktop && <ScramblerGameComponent />}
@@ -199,23 +199,7 @@ export default function ContentPage() {
                 </div>
 
 
-                <h1 className="mb-5 font-bold">SCRAMBLER THERAPY NEGLI STATI UNITI</h1>
 
-                <div className=" mx-5 mb-20 flex flex-col gap-3">
-
-                    {scramblerUsa.map((target, index) => {
-                        return (
-
-
-
-                            <h2 key={index + 1}>{target}</h2>
-
-
-
-                        )
-                    })}
-
-                </div>
             </TextContainer>
         </div >
 
